84da70471e4e099d63b8190602ef750bcd90a4d9
Tino Hager <tino.hager@nager.at>
2025-01-14 13:28:37 +0100

    Update quasar.config.ts

--- src/Frontend/quasar.config.ts
+++ src/Frontend/quasar.config.ts
@@ -53,7 +53,7 @@ export default defineConfig((/* ctx */) => {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      // publicPath: '/',
+      publicPath: '/RateCalculator/',
       // analyze: true,
       // env: {},
       // rawDefine: {}
@@ -64,7 +64,7 @@ export default defineConfig((/* ctx */) => {
 
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
-      
+
       vitePlugins: [
         ['vite-plugin-checker', {
           vueTsc: true,
